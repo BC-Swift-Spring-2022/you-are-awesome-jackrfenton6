4bd731dc7486f466c3a7f3d04561453cab73bf6d
Jack Fenton <jackrfenton6@gmail.com>
2022-01-31 16:13:34 -0500

    Section 2.11

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,25 +13,28 @@ class ViewController: UIViewController {
     
     var imageNumber = 0
     var messageNumber = 0
+    let totalNumberOfImages = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let messages = ["You are awesome!", "You are Great!", "You are Da Bomb!", "You are Fabulous"]
-        
-        messageLabel.text = messages[messageNumber]
-        messageNumber += 1
-        if messageNumber == messages.count {
-            messageNumber = 0
+        let messages = ["You are awesome!", "You are Great!", "You are Da Bomb!",
+                        "When the Genius Bar needs help they call you!", "You are Fabulous", "You have the design skills of Jony Ive"]
+        messageLabel.text = messages[Int.random(in: 0...messages.count-1)]
+        imageView.image = UIImage(named: "embiid\(Int.random(in: 0...totalNumberOfImages))")
+//        messageLabel.text = messages[messageNumber]
+//        messageNumber += 1
+//        if messageNumber == messages.count {
+//            messageNumber = 0
         }
         
-        let imageName = "embiid" + String(imageNumber)
-        imageView.image = UIImage(named: imageName )
-        imageNumber = imageNumber + 1
-        if imageNumber == 5 {
-            imageNumber = 0
+//        let imageName = "embiid" + String(imageNumber)
+//        imageView.image = UIImage(named: imageName )
+//        imageNumber = imageNumber + 1
+//        if imageNumber == 5 {
+//            imageNumber = 0
         }
         
         //        let awesomeMessage = "You are awesome!"
@@ -49,6 +52,6 @@ class ViewController: UIViewController {
         //            imageView.image = UIImage(named: "embiid")
         //        }
             
-    }
 
-}
+
+
