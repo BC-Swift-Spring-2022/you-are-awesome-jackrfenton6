5cae959e1afdfad6899fc2680f26bd901a5c0998
Jack Fenton <jackrfenton6@gmail.com>
2022-02-03 16:35:48 -0500

    2.11 updated

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     
     var imageNumber = 0
     var messageNumber = 0
-    let totalNumberOfImages = 5
+    let totalNumberOfImages = 6
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
         let messages = ["You are awesome!", "You are Great!", "You are Da Bomb!",
                         "When the Genius Bar needs help they call you!", "You are Fabulous", "You have the design skills of Jony Ive"]
         messageLabel.text = messages[Int.random(in: 0...messages.count-1)]
-        imageView.image = UIImage(named: "embiid\(Int.random(in: 0...totalNumberOfImages))")
+        imageView.image = UIImage(named: "embiid\(Int.random(in: 0...totalNumberOfImages-1))")
 //        messageLabel.text = messages[messageNumber]
 //        messageNumber += 1
 //        if messageNumber == messages.count {
